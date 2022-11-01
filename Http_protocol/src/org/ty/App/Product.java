75ba3de12b76472c298e09e94332e70ae28eebe7
arunelavarasan <arunelavarsan168@gmail.com>
2022-11-01 13:13:04 +0530

    Initial commit

---
+++ Http_protocol/src/org/ty/App/Product.java
@@ -0,0 +1,47 @@
+package org.ty.App;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+import jakarta.servlet.GenericServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Product extends HttpServlet{
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		String pi = req.getParameter("pid");
+		int pid = Integer.parseInt(pi);
+		String nm = req.getParameter("pname");
+		String price = req.getParameter("pprice");
+		String qty = req.getParameter("pqty");
+		try {
+			String inQry = "insert into product.product values(?,?,?,?)";
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306?user=root&password=root");
+			pstmt = con.prepareStatement(inQry);
+			pstmt.setInt(1, pid);
+			pstmt.setString(2, nm);
+			pstmt.setString(3, qty);
+			pstmt.setString(4, price);
+			pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+	}
+		
+		PrintWriter output = resp.getWriter();
+		output.print("<html><body bgcolor='cyan'>" + "<h1>Successfully Stored into Database!!!" + "</h1>" + "<h2>Details are:-</h2>" + "<h4>Id="
+				+ pid + "</h4>" + "<h4>Product=" + nm + "</h4>" + "<h4>Quality=" + qty + "</h4>" +"<h4>Price=" + price + "" + "</h4></body></html>");
+	
+	}	
+}
